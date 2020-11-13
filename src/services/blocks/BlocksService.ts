a55b8ab88130e3194098becef3196fa26f4e9cb7
Zeke Mostov <32168567+emostov@users.noreply.github.com>
2020-11-12 18:27:57 -0800

    Prevent calcFee from breaking when consts do not work

--- src/services/blocks/BlocksService.ts
+++ src/services/blocks/BlocksService.ts
@@ -335,11 +335,6 @@ export class BlocksService extends AbstractService {
 			parentParentHash = parentHash;
 		}
 
-		const perByte = api.consts.transactionPayment.transactionByteFee;
-		const extrinsicBaseWeight = api.consts.system.extrinsicBaseWeight;
-		const multiplier = await api.query.transactionPayment.nextFeeMultiplier.at(
-			parentHash
-		);
 		// The block where the runtime is deployed falsely proclaims it would
 		// be already using the new runtime. This workaround therefore uses the
 		// parent of the parent in order to determine the correct runtime under which
@@ -347,6 +342,21 @@ export class BlocksService extends AbstractService {
 		const version = await api.rpc.state.getRuntimeVersion(parentParentHash);
 		const specName = version.specName.toString();
 		const specVersion = version.specVersion.toNumber();
+
+		const perByte = api.consts?.transactionPayment?.transactionByteFee;
+		const extrinsicBaseWeight = api.consts?.system?.extrinsicBaseWeight;
+		const multiplier = await api.query?.transactionPayment?.nextFeeMultiplier?.at(
+			parentHash
+		);
+
+		if (!(perByte && extrinsicBaseWeight && multiplier)) {
+			return {
+				calcFee: null,
+				specName,
+				specVersion,
+			};
+		}
+
 		const coefficients = api.consts.transactionPayment.weightToFee.map(
 			(c) => {
 				return {
